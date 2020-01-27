1892b999a1e39ca2ef056cd2edd7d06cc23fa2e6
Brian Heise <bnheise@gmail.com>
2020-01-27 16:16:30 +0900

    made recipe list display

--- app.rb
+++ app.rb
@@ -2,11 +2,18 @@ require "sinatra"
 require "sinatra/reloader" if development?
 require "pry-byebug"
 require "better_errors"
+require_relative "recipe"
+require_relative "cookbook"
+
 configure :development do
   use BetterErrors::Middleware
   BetterErrors.application_root = File.expand_path('..', __FILE__)
 end
 
+csv_file_path = File.join(__dir__, 'recipes.csv')
+cookbook = Cookbook.new(csv_file_path)
+
 get '/' do
+  @cookbook = cookbook
   erb :index
 end
